1558bf5119ab0aff611eb12573dad19f756de0e3
Uyara Montovaneli <uyaramontovaneli@gmail.com>
2024-03-20 10:45:11 -0600

    edit on setting

--- app/src/main/java/com/example/spendwise/SettingsPage.kt
+++ app/src/main/java/com/example/spendwise/SettingsPage.kt
@@ -1,10 +1,191 @@
 package com.example.spendwise
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun SettingPage(){
-    Text(text = "Setting Page")
+fun SettingPage() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    val context = LocalContext.current.applicationContext
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 26.dp)
+            .wrapContentHeight(align = Alignment.CenterVertically),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.user),
+            style = TextStyle(
+                fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Text(
+            text = stringResource(id = R.string.edit_info),
+            style = TextStyle(
+                fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                fontSize = 20.sp,
+                color = Color.Gray
+            ),
+            modifier = Modifier.padding(bottom = 24.dp)
+        )
+        Card(
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier
+                .padding(vertical = 24.dp)
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        ) {
+            var showMessage by remember { mutableStateOf(false) }
+            var errorMessage by remember { mutableStateOf<String?>(null) }
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = { Text(text = stringResource(id = R.string.edit_Username)) },
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedLabelColor = Color(0xFF006A68),
+                        unfocusedLabelColor = Color(0xFF006A68),
+                        focusedIndicatorColor = Color(0xFF006A68),
+                        unfocusedIndicatorColor = Color(0xFF006A68)
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "Username",
+                            tint = Color(0xFF006A68)
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text(stringResource(id = R.string.edit_Password)) },
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedLabelColor = Color(0xFF006A68),
+                        unfocusedLabelColor = Color(0xFF006A68),
+                        focusedIndicatorColor = Color(0xFF006A68),
+                        unfocusedIndicatorColor = Color(0xFF006A68)
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Lock,
+                            contentDescription = "Password",
+                            tint = Color(0xFF006A68)
+                        )
+                    },
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = confirmPassword,
+                    onValueChange = { confirmPassword = it },
+                    label = { Text(stringResource(id = R.string.edit_ConfirmPassword)) },
+                    shape = RoundedCornerShape(20.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedLabelColor = Color(0xFF006A68),
+                        unfocusedLabelColor = Color(0xFF006A68),
+                        focusedIndicatorColor = Color(0xFF006A68),
+                        unfocusedIndicatorColor = Color(0xFF006A68)
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Lock,
+                            contentDescription = "Confirm Password",
+                            tint = Color(0xFF006A68)
+                        )
+                    },
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Button(
+                    onClick = {
+                        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                            errorMessage = "Fields are required"
+                        } else if (password != confirmPassword) {
+                            errorMessage = "Passwords do not match"
+                        } else {
+                            showMessage = true
+                            errorMessage = null
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(Color(0xFF006A68)),
+                    contentPadding = PaddingValues(
+                        start = 60.dp,
+                        end = 60.dp,
+                        top = 8.dp,
+                        bottom = 8.dp
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 24.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.btn_editAccount),
+                        fontSize = 22.sp
+                    )
+                }
+
+                errorMessage?.let {
+                    Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                }
+
+                if (showMessage) {
+                    Toast.makeText(context, "Edit saved", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 }
